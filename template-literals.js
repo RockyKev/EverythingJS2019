6de95d1992e5871a6930f06a50d53056408b39bb
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2019-06-14 22:22:00 -0700

    06-14-2019

---
+++ template-literals.js
@@ -0,0 +1,27 @@
+//template literals
+//https://drive.google.com/drive/folders/1B-eWVHXw6roKhQWpRkiNNwHtb-66Gzif PAGE 70
+
+const string1 = `First
+
+                Second`
+
+console.log(string1)
+
+const string2 = 
+`Third
+Fourth`.trim()
+
+console.log(string2)
+
+const string3 = `
+              Fifth
+              Sixth`.trim()
+
+console.log(string3)
+
+//Interpolation
+
+
+const myName = "rocky"
+
+console.log(`There he goes, his name is ${myName}`)
