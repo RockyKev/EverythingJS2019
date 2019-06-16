f2df30d29f876296b8be1b474f67547657c28f7c
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2019-06-16 16:18:36 -0700

    added on 2019-06-16

---
+++ event-handlers.js
@@ -0,0 +1,12 @@
+//pg 101
+//https://drive.google.com/drive/folders/1B-eWVHXw6roKhQWpRkiNNwHtb-66Gzif
+
+
+// BEFORE:
+// < a href="site.com" onclick="doSomething();"> a link </a>
+
+
+// NOW: 
+// window.addEventListener('load', () =>  {
+
+// })
