fa4187e4559328b74e848b16c6fa5b6ffea76a55
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2019-06-21 18:41:24 -0700

    Create function-defaults.js

---
+++ function-defaults.js
@@ -0,0 +1,18 @@
+//function default parameters
+
+// function setColor(element, color = 'pink') {
+//   element.style.backgroundColor = color
+
+// }
+
+// setColor(divA)
+// setColor(divB, undefined)
+// setColor(divC, 'blue')
+
+function cube(number = 0) {
+  return number * number * number
+}
+
+console.log(cube(0)) // 0
+console.log(cube(3)) // 27
+
