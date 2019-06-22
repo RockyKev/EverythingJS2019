e1d2db903e2be70ce14eaa3cf3dcbad4985604ec
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2019-06-21 18:41:51 -0700

    Create async.js

---
+++ async.js
@@ -0,0 +1,12 @@
+//async recipes
+
+//async function & await keyword
+
+//asynch - describes tasks occuring out of order. The main program can fcontinue now, and async task will complete later. 
+
+//Parallel means multiple thread
+//JS is single-threaded, run to completion.
+
+//Concurrency is two or more tasks running in the same time period
+
+//JS has race condition
