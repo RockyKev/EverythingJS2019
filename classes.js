9006813282f13cc76c0bb8cba84f006a29839225
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2019-07-05 16:11:58 -0700

    Update and rename classes 2019-06-08.js to classes.js

--- classes.js
+++ classes.js
@@ -7,7 +7,7 @@
 // class Actor extends Person {  hello() {    return super.hello() + ' I am an actor.'  }}
 
 // var tomCruise = new Actor('Tom Cruise')tomCruise.hello()
-
+ 
 class Person {
   constructor(name) {
     this.name = name
