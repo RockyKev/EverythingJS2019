be5abe556834f3360c7fbbbe0427d7dc4facac81
Rocky Kev <6101650+RockyKev@users.noreply.github.com>
2019-08-11 22:11:55 -0700

    Create type-cohesion.js

---
+++ type-cohesion.js
@@ -0,0 +1,10 @@
+// Implicit
+console.log("20" + 18);                   // Logs: 2018
+console.log("20" * 18);                   // Logs: 360
+console.log(20 + true);                   // Logs: 21
+console.log("20" == 20);                  // Logs: true
+console.log("20" === 20);                 // Logs: false
+
+// Explicit
+console.log(Number("20") + 18);           // Logs: 38
+console.log(String(20) + String(true));   // Logs: "20true"
